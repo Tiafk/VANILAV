d9f980c613aeda5a6b06ad0c004734cf5f22f890
Tiafk <akchurin227@mail.ru>
2025-06-17 09:51:01 +0300

    location+block8 img

--- location.js
+++ location.js
@@ -1,14 +1,35 @@
 //кондитерские
 
-function createCard({ city, address, phone, hours }) {
+function createCard({ city, address, phone, hours, hours2, link, title }) {
   const card = document.createElement('div');
-  card.className = 'location-card'; // Новый класс
+  card.className = 'location-card';
 
+  // Формируем блок title только если он задан
+  const titleHTML = title ? `<p class="title">${title}</p>` : '';
+  let hoursHTML = '';
+  if (hours && hours2) {
+    // Два времени — в колонку
+    hoursHTML = `
+      <p class="text">
+        <strong class="bold">Режим работы:</strong><br>${hours}<br>${hours2}
+      </p>
+    `;
+  } else if (hours) {
+    // Только одно время — в строку
+    hoursHTML = `
+      <p class="text">
+        <strong class="bold">Режим работы:</strong> ${hours}
+      </p>
+    `;
+  }
   card.innerHTML = `
-    <p class="text"><strong class="bold">Город:</strong> ${city}</p>
-    <p class="text"><strong class="bold">Адрес:</strong> ${address}</p>
-    <p class="text"><strong class="bold">Телефон:</strong> ${phone}</p>
-    <p class="text"><strong class="bold">Режим работы:</strong> ${hours}</p>
+    <a class="location-card-link" href="${link}">
+      ${titleHTML}
+      <p class="text"><strong class="bold">Город:</strong> ${city}</p>
+      <p class="text"><strong class="bold">Адрес:</strong> ${address}</p>
+      <p class="text"><strong class="bold">Телефон:</strong> ${phone}</p>
+      ${hoursHTML}
+    </a>
   `;
 
   return card;
@@ -17,6 +38,7 @@ function createCard({ city, address, phone, hours }) {
 function renderCards(dataArray) {
   const container = document.getElementById('card-container');
   container.innerHTML = '';
+
   dataArray.forEach(data => {
     const card = createCard(data);
     container.appendChild(card);
@@ -31,3 +53,4 @@ fetch('./location.json')
   .catch(error => {
     console.error('Ошибка при загрузке JSON:', error);
   });
+
